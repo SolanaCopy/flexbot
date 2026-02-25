c8d9d676a1372fb35bbadae09f970f4475d75378
Flexbot <flexbot@users.noreply.github.com>
2026-02-25 03:54:37 +0100

    Adjust custom1 mascot position

--- server.js
+++ server.js
@@ -4106,8 +4106,7 @@ function createClosedCardSvgV3({ id, symbol, direction, outcome, result, entry, 
   // Add entries like: "mascot_win_custom8.png": { x:-180, y:420, w:820, h:920 }
   const mascotOverrides = {
     // win
-    // "mascot_win_custom1.png": { x: -160, y: 400, w: 840, h: 940 },
-    // "mascot_win_custom8.png": { x: -160, y: 400, w: 840, h: 940 },
+    "mascot_win_custom1.png": { x: -140 },
     // loss
     // "mascot_loss_force.png": { x: -160, y: 400, w: 840, h: 940 },
   };
